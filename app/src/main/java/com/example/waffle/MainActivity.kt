c1de928705205f313a0ebf2d88a1b763f052e3a8
Clarence <clarencepenz@users.noreply.github.com>
2024-06-08 19:40:22 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/waffle/MainActivity.kt
+++ app/src/main/java/com/example/waffle/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         val activityResultSender = ActivityResultSender(this)
+
         
         setContent {
             WaffleTheme {
